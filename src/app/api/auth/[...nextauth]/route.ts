dc5f5661d99e38b71046daa9a50a10d70dccadb3
Prudvi0033 <prudvirajvarma2004@gmail.com>
2025-07-24 23:41:29 +0530

    Added signup

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
+import prisma from "@/app/lib/prisma";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -8,6 +8,28 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks : {
+    //@ts-ignore
+    async signIn({user}){
+      
+      if(!user.email){
+        return false
+      }
+      try {
+        await prisma.listner.create({
+          data: {
+            email: user.email,
+            provider: "Google"
+          }
+        })
+      } catch (error) {
+        return Response.json({
+          message :"Error in adding user"
+        })
+      }
+      return true;
+    }
+  },
 };
 
 const handler = NextAuth(authOptions)
